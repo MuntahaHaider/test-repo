0461998e51e53e043436263a5dc660249199fd96
MuntahaHaider <160015309+MuntahaHaider@users.noreply.github.com>
2024-06-29 14:10:19 +0500

    Add files via upload

---
+++ React js/reactAssignment05/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const App = ()=>{
+    return(
+        <div>
+            <h1>Hello World</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In consequuntur ipsum illum qui. A, deserunt mollitia expedita dolor, placeat sapiente exercitationem quo praesentium nulla nesciunt quisquam numquam adipisci repellat? Pariatur quam adipisci amet voluptatum obcaecati quaerat mollitia, atque placeat eligendi!</p>
+            <img src="/vite.svg" alt="" />
+        </div>
+    )
+}
+export {App}
